e4ea781f2cf9e3328103eb469ca3c327e9eb2835
LeLouLamb <112926148+LeLouLamb@users.noreply.github.com>
2024-11-16 13:23:20 -0700

    calibrate

--- ICM_3D.py
+++ ICM_3D.py
@@ -1,5 +1,5 @@
 from reg_func import *
-
+import time
 class ICM_3D:
   def __init__(self, bus, dev_add, start_reg, reg_scaler):
     self.x_h_reg = start_reg
@@ -12,9 +12,7 @@ class ICM_3D:
     self.dev = dev_add
     self.bus = bus
 
-    self.x_tare = self.x
-    self.y_tare = self.y
-    self.z_tare = self.z
+    self.calibrate()
 
   @property
   def x(self):
@@ -26,8 +24,24 @@ class ICM_3D:
     
   @property
   def z(self):
-    return self.scaler * (read_signed_16(self.bus, self.dev, self.y_h_reg, self.y_l_reg) - self.z_tare)
+    return self.scaler * (read_signed_16(self.bus, self.dev, self.z_h_reg, self.z_l_reg) - self.z_tare)
     
+  def calibrate(self):
+    num_samples = 0x40
+    self.x_tare = 0
+    self.y_tare = 0
+    self.z_tare = 0
+    for _ in range(num_samples):
+      self.x_tare += self.x
+      self.y_tare += self.y
+      self.z_tare += self.z
+
+      time.sleep(0.01)
+
+    self.x_tare //= num_samples
+    self.y_tare //= num_samples
+    self.z_tare //= num_samples
+
   def print(self):
-    print(f"X={self.x:.2f}, Y={self.y:.2f}, Z={self.z:.2f}")
+    return f"X={self.x:.2f}, Y={self.y:.2f}, Z={self.z:.2f}"
 
